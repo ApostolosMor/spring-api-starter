4be1386a2560d16316f8a16f1730b6f96e2f9aeb
ContemporaryApostolos <securersecurly@gmail.com>
2025-09-25 12:34:05 +0300

    Configure production properties for Railway

---
+++ src/main/java/com/springproject/store/payments/CheckoutService.java
@@ -0,0 +1,70 @@
+package com.springproject.store.payments;
+
+
+import com.springproject.store.entities.Order;
+import com.springproject.store.exceptions.CartEmptyException;
+import com.springproject.store.exceptions.CartNotFoundException;
+import com.springproject.store.repositories.CartRepository;
+import com.springproject.store.repositories.OrderRepository;
+import com.springproject.store.services.AuthService;
+import com.springproject.store.services.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CheckoutService {
+
+    private final CartRepository cartRepository;
+    private final AuthService authService;
+    private final OrderRepository orderRepository;
+    private final CartService cartService;
+    private final PaymentGateway paymentGateway;
+
+
+
+    @Transactional
+    public CheckoutResponse checkout(CheckoutRequest request) {
+
+
+        var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
+
+        if (cart == null){
+            throw new CartNotFoundException();
+        }
+
+        if (cart.isEmpty()){
+            throw new CartEmptyException();
+
+        }
+
+        // You must fetch the user object from the repository
+        var order = Order.fromCart(cart, authService.getCurrentUser());
+
+        orderRepository.save(order);
+
+        try{
+            //Checkout session
+            var session = paymentGateway.createCheckoutSession(order);
+
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(), session.getCheckoutUrl());
+        }catch(PaymentException ex){
+            orderRepository.delete(order);
+            throw ex;
+        }
+    }
+
+    public void handleWebhookEvent(WebhookRequest request){
+        paymentGateway.parseWebhookRequest(request)
+                .ifPresent(paymentResult -> {
+                    var order = orderRepository.findById(paymentResult.getOrderId()).orElseThrow();
+                    order.setStatus(paymentResult.getPaymentStatus());
+                    orderRepository.save(order);
+                });
+
+
+    }
+}
---
+++ src/main/java/com/springproject/store/payments/PaymentResult.java
@@ -0,0 +1,12 @@
+package com.springproject.store.payments;
+
+import com.springproject.store.entities.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PaymentResult {
+    private Long orderId;
+    private PaymentStatus paymentStatus;
+}
